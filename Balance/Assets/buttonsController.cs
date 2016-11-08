3fe8ee50ad2fef6cbffb75a1948637e2019a5765
Tarek <tareksaidee@hotmail.com>
2016-11-08 00:31:09 -0500

    Add all changes up to now

---
+++ Balance/Assets/buttonsController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class buttonsController : MonoBehaviour {
+
+	public void startNewGame()
+    {
+        SceneManager.LoadScene("testScene");
+    }
+}
