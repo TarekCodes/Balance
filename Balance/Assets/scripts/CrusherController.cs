ad2527b2649c6c2f9b45d7f71896798df5cd9be6
Tarek <tareksaidee@hotmail.com>
2016-11-26 01:10:41 -0500

    Add level 6 basic obstacles

---
+++ Balance/Assets/scripts/CrusherController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrusherController : MonoBehaviour {
+    Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(0, -7f);
+        print(transform.position.y);
+	}
+
+    void Update()
+    {
+    }
+
+        // Update is called once per frame
+        void FixedUpdate () {
+        if (transform.position.y < 6)
+        {
+            print(transform.position.y);
+            rigid.velocity = new Vector2(0, 7f);
+        }
+        else if (transform.position.y > 12)
+        {
+            print(transform.position.y);
+            rigid.velocity = new Vector2(0, -7f);
+        }
+	}
+}
