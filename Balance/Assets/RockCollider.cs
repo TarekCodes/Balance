9b4f63a45b5f344d811f6415f3f023bf23d79827
Tarek <tareksaidee@hotmail.com>
2016-11-25 23:42:21 -0500

    Add level 6 rock script & anim

---
+++ Balance/Assets/RockCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(15f, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (GetComponent<Rigidbody2D>().velocity.x < 15f)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(9f, 0);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "mountain" && !coll.gameObject.CompareTag("platform"))
+            Destroy(coll.gameObject);
+
+    }
+}
