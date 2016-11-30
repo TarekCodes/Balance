f8b22ee5a484607f426963d7d916a4583477b857
Tarek <tareksaidee@hotmail.com>
2016-11-29 23:45:48 -0500

    Add new obstacles to level 6

---
+++ Balance/Assets/scripts/GateController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateController : MonoBehaviour {
+
+    public bool clicked = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        
+       
+    }
+}
---
+++ Balance/Assets/scripts/SwitchController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject obj = GameObject.FindGameObjectWithTag("gate");
+            Destroy(obj);//.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -7f);
+        }
+    }
+}
